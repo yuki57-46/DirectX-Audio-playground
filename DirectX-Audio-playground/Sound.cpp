6920872f4678b6fdedc65c2f49d728d641ca5043
yuki57-46 <yuki.rno@gmail.com>
2023-11-09 02:49:42 +0900

    Revert "ヘッダーに構造体を追加"
    This reverts commit 034e6800c6d8a839136f98604b46736fdceae38e.

--- DirectX-Audio-playground/Sound.cpp
+++ DirectX-Audio-playground/Sound.cpp
@@ -12,43 +12,6 @@
 #pragma comment(lib, "msacm32.lib") // ファイルの読み込みに使用
 #pragma comment(lib, "shlwapi.lib") // ファイルパスの取得に使用
 
-//-----------------
-// プロトタイプ宣言
-//-----------------
-HRESULT LoadWave(const char* file, Sound::SoundData *pData);
-HRESULT LoadMP3(const char* file, Sound::SoundData *pData);
-DWORD ReadMP3Format(HANDLE hFile, Sound::MP3FormatInfo *pFormat);
-DWORD ReadMP3FrameHeader(HANDLE hFile, DWORD seek, Sound::MP3FrameInfo *pFrame);
-DWORD ReadMP3Data(HANDLE hFile, DWORD seek, DWORD size, Sound::MP3FrameInfo *pFrame, Sound::SoundData *pData);
-
-//--------------
-// 定数定義
-//--------------
-const BYTE CMP_MATCH = 0; // memcmpの戻り値が一致した場合
-
-//-----------------
-// グローバル変数
-//-----------------
-
-// std::pair 役割: 2つの値をまとめて扱う
-// std::string 役割: 文字列を扱う
-// Sound::SoundData 役割: サウンドデータを扱う
-// SoundKey 役割: サウンドデータを扱う
-typedef std::pair<std::string, Sound::SoundData> SoundKey;
-// std::map 役割: 連想配列を扱う
-// SoundKey 役割: サウンドデータを扱う
-// Sound::SoundData 役割: サウンドデータを扱う
-// SoundList 役割: サウンドデータを扱う
-typedef std::map<std::string, Sound::SoundData> SoundList;
-
-IXAudio2 *g_pXAudio;
-IXAudio2MasteringVoice *g_pMasteringVoice;
-SoundList g_SoundList;
-
-/**
- * @brief 初期化処理
- * @return 処理結果(sccess: S_OK, failed: E_FAIL)
- */
 HRESULT Sound::Init()
 {
 	return E_NOTIMPL;
@@ -62,7 +25,6 @@ XAUDIO2_BUFFER* Sound::LoadSound(const char* file, bool loop)
 {
 	return nullptr;
 }
-
 
 IXAudio2SourceVoice* Sound::PlaySound(XAUDIO2_BUFFER* pSound)
 {
